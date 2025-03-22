28863027333caf8c31e3a6b1ccbf3faa9049cfaa
vdi-terminal <vdi-terminal@EDU-WSB.local>
2025-03-22 10:17:40 +0100

    poprawka nr 5

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -23,7 +23,6 @@ def get_formatted(msg, imie, format):
 def format_to_json(msg, imie):
     return '{ "imie": "' + imie + '", "msg": "' + msg + '" }'
 
-
 def plain_text(msg, imie):
     return imie + ' ' + msg
 
